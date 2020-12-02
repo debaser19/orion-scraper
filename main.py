d18f16ecb0333fc8066cff2cd3dbb4572deabbd1
Steve Stoveld <sstoveld@888voip.com>
2020-12-02 17:04:42 -0500

    added mfa login

--- main.py
+++ main.py
@@ -12,11 +12,18 @@ def scrape_orion():
     driver.get(f'{instances_url}{sandbox_org}/instances')
 
     # log in using the form
-    # TODO: need to get the actual creds and update in creds.py
     driver.find_element_by_xpath('//*[@id="user_login"]').send_keys(creds.creds['username'])
     driver.find_element_by_xpath('//*[@id="user_password"]').send_keys(creds.creds['password'])
     driver.find_element_by_xpath('//*[@id="new_user"]/div[3]/div/input').click()
+
+    # ask for MFA code
+    mfa_code = input("Please enter the MFA code: ")
+    driver.find_element_by_xpath('//*[@id="code"]').send_keys(mfa_code)
+    driver.find_element_by_xpath('/html/body/main/div/section/div[2]/div/form/div[3]/div/input').click()
     time.sleep(10)
+
+    # select the reseller dropdown and grab all hrefs
+    reseller_ul = driver.find_element_by_xpath('//*[@id="dropdown"]/div[1]/ul').click()
 
 
 if __name__ == '__main__':
